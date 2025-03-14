695d33ae9447ddd7f70b9306f4817f6de8968149
Propehcy <126004186+Propehcy@users.noreply.github.com>
2025-03-14 15:30:03 +0100

    Add files via upload

---
+++ scripts/cc1.js
@@ -0,0 +1,58 @@
+function retour_villes(le_dep)
+{
+	var chaine_villes="";
+	
+	switch(le_dep)
+	{
+		case "Astral + Mephisto (sans Moon Dragon)":
+		chaine_villes = "2 suicides + Nightstalkers clean up ▲|2 suicides + IX clean up ▲▲▲";
+		break;
+		case "Conscience collective + Sasquatch (sans Venom)":
+		chaine_villes = "Superior 6 + Vautour▲▲▲";
+		break;
+		case "Illuminati + Thanos EG (sans Mister Fantastic)":
+		chaine_villes = "Absolute A-Force suicide + clean up Astral ▲▲▲|PF + Logan + Dormamu + Diablo ▲▲|Cabale + Kang + Odin ▲▲";
+		break;
+		case "Imperissables + Robbie":
+		chaine_villes = "Cabale + Kang + Vautour ▲▲▲";
+		break;
+		case "Liberty": 
+		chaine_villes = "Superior 6 + Vautour (sans Robot) ▲ + clean up";
+		break; /* Orchis */
+		case "Mephisto + Thanos EG + SK + Kahhori + Miss Marvel Classic": 
+		chaine_villes = "2 suicides + Odin et un pre taunt clean up ▲|2 suicides + Nightstalkers clean up ▲";
+		break; /* Orchis */
+		case "Mighty AV": 
+		chaine_villes = "Superior 6 + Vautour▲▲▲";
+		break; /* Orchis */
+		case "Mighty AV + Morgan (sans Hercules)": 
+		chaine_villes = "Superior 6 + Vautour (sans Robot) ▲▲▲|IX ▲▲▲";
+		break; /* Orchis */
+		case "Mighty AV + Dormamu + Quicksilver (sans Hercules ni Faucon)": 
+		chaine_villes = "IX ▲▲▲";
+		break; /* Orchis */
+		case "Odinati (sans BB)": 
+		chaine_villes = "PF + Apo + BK + Echo ▲ + clean up ";
+		break; /* Orchis */
+		case "Odinati (sans Fantastic)": 
+		chaine_villes = "PF + Apo + BK + Echo ▲ + clean up|Mephisto + PF + Apo + Kahhori ▲▲▲|Mephisto + Guardian + Echo + BK + Diablo ▲▲▲|Mephisto + PF + BK + Echo ▲▲▲";
+		break; /* Orchis */
+		case "Odinati + BK (sans BB ni Fantastic)": 
+		chaine_villes = "PF + Apo + BK + Echo ▲ + clean up|2 suicides + Annihilateurs Odin (sans Sufer) clean up ▲▲";
+		break; /* Orchis */
+		case "Orchis": 
+		chaine_villes = "Nightstalkers ▲▲▲";
+		break; /* Orchis */
+		case "Secret Defenders": 
+		chaine_villes = "New Warriors ▲▲(One shot Strange)";
+		break; /* Orchis */
+		case "Superior 6": 
+		chaine_villes = "Cabale + Kang + Titania ▲";
+		break; /* Orchis */
+					
+	}
+	
+	return chaine_villes;
+}
+
+|
---
+++ scripts/cc2.js
@@ -0,0 +1,41 @@
+function retour_villes(le_dep)
+{
+	var chaine_villes="";
+	
+	switch(le_dep)
+	{
+		case "Astral":
+		chaine_villes = "IX ▲▲▲|Cabale + Kang + Dormamu ▲▲▲";
+		break;
+		case "Astral + Dormamu":
+		chaine_villes = "Mercenaires d'argent + Noir (sans Deathpool) ▲▲";
+		break;
+		case "Gambit + Sunfire + Rogue + Forge + Cyclope":
+		chaine_villes = "Superior 6 + Vautour (sans Robot)▲▲";
+		break;
+		case "Spiders Society":
+		chaine_villes = "Superior 6 + Vautour (sans Robot)▲▲▲|Secret Defenders + Cosmic Ghost Rider (no Strange) ▲";
+		break;
+		case "Spiders Society + Daken (sans Ghost)": 
+		chaine_villes = "Logan + Pandapool + Rogue + Miss Marvel Hardlight + Black Cat ▲▲▲";
+		break; /* Orchis */
+		case "Spiders Society + Mephisto (sans Ghost)": 
+		chaine_villes = "2 attaques suicides + Secrets defenders + Cosmic rider (sans Strange) ▲▲|PF + Apo + Mephisto + Sasquatch ▲▲▲";
+		break; /* Orchis */
+		case "Spiders Society + Mephisto (sans Noir)": 
+		chaine_villes = "Superior 6 + Odin (sans Doc Oc) ▲";
+		break; /* Orchis */
+		case "Spiders Society + Sasquatch": 
+		chaine_villes = "2 suicides + Secrets Defenders + CGR clean up ▲▲▲";
+		break; /* Orchis */
+		case "Spiders Society + Weaver (sans Ghost)": 
+		chaine_villes = "IX ▲▲▲|Superior 6 ▲▲▲";
+		break; /* Orchis */
+		case "Spiders Society + Weaver (sans Pavitr)": 
+		chaine_villes = "Superior 6 ▲▲▲|Superior 6 + Vautour (sans robot) ▲|Superior 6 + Mysterio (sans robot) ▲▲▲|IX ▲▲▲";
+		break; /* Orchis */
+					
+	}
+	
+	return chaine_villes;
+}
